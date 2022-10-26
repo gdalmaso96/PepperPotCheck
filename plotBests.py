2f3ddbafda7780d21bc99274cd56b3ef705612b7
gdalmaso96 <g.dalmaso@outlook.it>
2022-10-26 18:17:17 +0200

    Added python macro to plot comparison btw simulation data and LL. The simulations must already exist and the name of the scores is the only parameter to be provided.

---
+++ plotBests.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.insert(1, '/Users/giovanni/PhD/Simulazioni/BeamTimeMEG/2022_06/PepperPotCheck/include')
+from BeamData import BeamData
+import time
+start_time = time.time()
+import matplotlib.pyplot as plt
+import optuna
+import numpy as np
+
+data = BeamData("configurations.yaml", "docker exec -ti --workdir=/home/developer/Simulazioni/BeamTimeMEG/2022_06/PepperPotCheck/g4bl g4bl /bin/bash -c 'source /home/developer/.bashrc && g4bl COMMAND'")
+
+#data.PlotBest("newH")
+data.PlotBest("bestCent")
