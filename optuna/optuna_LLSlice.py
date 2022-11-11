dde9faa369c7d0649257f4ece0d9c4e6114139d5
Dal Maso Giovanni <dalmaso_g@merlin-l-01.psi.ch>
2022-11-11 15:39:58 +0100

    Adapted to Merlin

--- optuna/optuna_LLSlice.py
+++ optuna/optuna_LLSlice.py
@@ -8,6 +8,7 @@ import subprocess
 import time
 import os
 import warnings
+from optuna.samplers import NSGAIISampler
 start_time = time.time()
 warnings.filterwarnings("ignore")
 
@@ -15,7 +16,7 @@ warnings.filterwarnings("ignore")
 # Fits the phase space centroids, longitudinal phase space and the Peper Pot horixontal slice in x = -6.25mm
 # 8 parameters: 4 centroids + 4 parameters of the momentum spectrum parametrization + 6 paramters of the slice
 
-nTrials = 3
+nTrials = 10 #3
 nJobs = 1
 slicex_X = [{'x': -6.25, 'Amin': 2.2, 'Amax': 3.0}]
 slicey_Y = []
@@ -38,11 +39,13 @@ def objective(trial):
 	#b = 27.8995
 	#c = 1.23719
 	#d = 0.05
-	x = trial.suggest_uniform('x', -100, 100)
-	xp = trial.suggest_uniform('xp', -500, 500)
+	megx = trial.suggest_uniform('megx', -100, 100)
+	megxp = trial.suggest_uniform('megxp', -500, 500)
+	mu3ex = trial.suggest_uniform('mu3ex', -100, 100)
+	mu3exp = trial.suggest_uniform('mu3exp', -500, 500)
 	y = trial.suggest_uniform('y', -100, 100)
 	yp = trial.suggest_uniform('yp', -500, 500)
-	beam = [a, b, c, d, x, xp, y, yp]
+	beam = [a, b, c, d, megx, megxp, mu3ex, mu3exp, y, yp]
 
 	slicex = []
 	for X in slicex_X:
@@ -66,19 +69,20 @@ def objective(trial):
 		temp['l1'] = trial.suggest_uniform('l1_y%d' %(len(slicey)), -10, 30)
 		temp['s2'] = trial.suggest_uniform('s2_y%d' %(len(slicey)), 40, 80)
 		temp['l2'] = trial.suggest_uniform('l2_y%d' %(len(slicey)), -20, 10)
-		slicey.append(temp)
 	
 	beam = {'beam': beam, 'slicex': slicex, 'slicey': slicey}
-	result0 = DATA.RunTrialSlice(beam, 100000, "out_%d_" %(trial.number), "beam_%d" %(trial.number))
+	result0 = DATA.RunTrialSlice(beam, 100000, "out_%d_" %(trial.number), "beam_%d" %(trial.number)) #, False)
 	
 	return result0
 
 if __name__ == '__main__':
 	args = sys.argv[1:]
 	
-	study = optuna.create_study(study_name="LL", storage='sqlite:////data/project/general/muonGroup/simulations/giovanni/PepperPotCheck/DBs/LL.db', direction='minimize', load_if_exists=True)
+	sampler = NSGAIISampler(population_size=50)
+	storage = optuna.storages.RDBStorage(url='sqlite:////data/project/general/muonGroup/simulations/giovanni/PepperPotCheck/DBs/LLSlice.db', engine_kwargs={"connect_args": {"timeout": 1000}})
+	study = optuna.create_study(study_name="LLSlice", storage=storage, sampler=sampler, direction='minimize', load_if_exists=True)
 	if len(args) == 1:
-		firstTry = {'a' : 0.0812462, 'b' : 27.8995, 'c' : 1.23719, 'd' : 0.05, 'x' : 0, 'xp' : 0, 'y' : 0, 'yp' : 0, 'A_x0' : 2.4, 'mu_x0' : -1, 's1_x0' : 72, 'l1_x0' : 0, 's2_x0' : 83, 'l2_x0' : -9}
+		firstTry = {'a' : 0.0812462, 'b' : 27.8995, 'c' : 1.23719, 'd' : 0.05, 'megx' : 0, 'megxp' : 0, 'mu3ex' : 0, 'mu3exp' : 0, 'y' : 0, 'yp' : 0, 'A_x0' : 2.4, 'mu_x0' : -1, 's1_x0' : 72, 'l1_x0' : 0, 's2_x0' : 83, 'l2_x0' : -9}
 		study.enqueue_trial(firstTry)
 	study.optimize(objective, n_trials=nTrials, n_jobs=nJobs)
 
