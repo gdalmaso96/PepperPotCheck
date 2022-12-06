Fix to Chi2 without normalization

include/BeamData.py
@@ -876,7 +876,7 @@ class BeamData:
 					else:
 						for (x,y) in zip(profile['profile'][0], profile['profile'][1]):
 							tmp = (np.abs(s-x) < 1).sum()
-							y1 = tmp/2 # each bin is 2 mm wide
+							y1 = tmp/2/len(s) # each bin is 2 mm wide
 							dy1 = np.sqrt(tmp)/2
 							#if dy1 > 0:
 							#	Chi2 += (y1 - y)**2/dy1**2/len(s)
@@ -891,7 +891,7 @@ class BeamData:
 					else:
 						for (x,y) in zip(profile['profile'][0], profile['profile'][1]):
 							tmp = (np.abs(s-x) < 1).sum()
-							y1 = tmp/2 # each bin is 2 mm wide
+							y1 = tmp/2/len(s) # each bin is 2 mm wide
 							dy1 = np.sqrt(tmp)/2
 							#if dy1 > 0:
 							#	Chi2 += (y1 - y)**2/dy1**2/len(s)
@@ -910,7 +910,7 @@ class BeamData:
 					else:
 						for (x,y,z) in zip(profile['profile'][0], profile['profile'][1], profile['profile'][2]):
 							tmp = ((np.abs(sx-x) < 1)*(np.abs(sy-y) < 1)).sum()
-							z1 = tmp/4 # each bin is 2 mm wide
+							z1 = tmp/4/len(s) # each bin is 2 mm wide
 							dz1 = np.sqrt(tmp)/4
 							#if dz1 > 0:
 							#	Chi2 += (z1 - z)**2/dz1**2/len(sx)